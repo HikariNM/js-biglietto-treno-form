ad4526af1181d9dbd8130fbc78fffaf5a19fb1ca
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-13 18:56:42 +0100

    aggiunto to fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-const under18 = 0.20;
-const over65 = 0.40;
-const priceKm = 0.21;
-
 const passengerName = document.getElementById('passengerName');
 const ticketCategory = document.getElementById('ticketCategory');
 const trainWagon = document.getElementById('trainWagon');
@@ -11,9 +7,9 @@ const ticketPrice = document.getElementById('ticketPrice');
 function generateTicketCode(){
     const char = 'ABCDEFGHILMNOPQRSTUVZ0123456789'
     let result = '';
-
+    
     for (let i = 0; i < 6; i++) {
-         result += char.charAt(Math.floor(Math.random() * char.length));
+        result += char.charAt(Math.floor(Math.random() * char.length));
         
     }
     return result
@@ -21,25 +17,25 @@ function generateTicketCode(){
 
 document.getElementById('ticketForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    // debugger
-    let name = document.getElementById('inputName').value;
+    debugger
+    let inputName = document.getElementById('inputName').value;
     let inputKm = parseFloat(document.getElementById('inputKm').value);
     let ticketDiscount = document.getElementById('ageSelect').value;
-    const standardPrice = inputKm * priceKm;
-    let result = standardPrice;
+    const standardPrice = (inputKm * 0.21).toFixed(2);
+    let price = standardPrice;
     let ticketType = 'Tariffa Standard';
     
     if (ticketDiscount === 'under18') {
-        result = standardPrice - (standardPrice * under18);
+        price = (standardPrice - (standardPrice * 0.20)).toFixed(2);
         ticketType = 'Sconto minorenni'
     } else if (ticketDiscount === 'over65') {
-        result = standardPrice - (standardPrice * over65);
+        price = (standardPrice - (standardPrice * 0.40)).toFixed(2);
         ticketType = 'Sconto Senior'
     }
 
-    passengerName.innerText = name;
+    passengerName.innerText = inputName;
     ticketCategory.innerText = ticketType;
-    ticketPrice.innerText = result + ('€');
+    ticketPrice.innerText = price + ('€');
     trainWagon.innerText = Math.floor(Math.random()* (6 - 1 + 1) + 1);
     ticketCode.innerText = generateTicketCode();
 
